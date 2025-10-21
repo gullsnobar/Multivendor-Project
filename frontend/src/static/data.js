96ca1591839adf72ed112a8e4a7b0f036b2be185
Gull Snobar <gullsnobar07@gmail.com>
2025-10-21 18:10:07 +0500

    Remove node_modules from repo

---
+++ frontend/src/static/data.js
@@ -0,0 +1,90 @@
+// navigation Data
+export const navItems = [
+  { title: "Home", url: "/" },
+  { title: "Best Selling", url: "/best-selling" },
+  { title: "Products", url: "/products" },
+  { title: "Events", url: "/events" },
+  { title: "FAQ", url: "/faq" },
+];
+
+// branding data
+export const brandingData = [
+  {
+    id: 1,
+    title: "Free Shipping",
+    Description: "From all orders over 100$",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1H5.63636V24.1818H35"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+        <path
+          d="M8.72763 35.0002C10.4347 35.0002 11.8185 33.6163 11.8185 31.9093C11.8185 30.2022 10.4347 28.8184 8.72763 28.8184C7.02057 28.8184 5.63672 30.2022 5.63672 31.9093C5.63672 33.6163 7.02057 35.0002 8.72763 35.0002Z"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+        <path
+          d="M31.9073 35.0002C33.6144 35.0002 34.9982 33.6163 34.9982 31.9093C34.9982 30.2022 33.6144 28.8184 31.9073 28.8184C30.2003 28.8184 28.8164 30.2022 28.8164 31.9093C28.8164 33.6163 30.2003 35.0002 31.9073 35.0002Z"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+        <path
+          d="M34.9982 1H11.8164V18H34.9982V1Z"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+        <path
+          d="M11.8164 7.18164H34.9982"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    title: "Daily Surprise Offers",
+    Description: "Save up to 25% off",
+    icon: (
+      <svg
+        width="32"
+        height="34"
+        viewBox="0 0 32 34"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M31 17.4502C31 25.7002 24.25 32.4502 16 32.4502C7.75 32.4502 1 25.7002 1 17.4502C1 9.2002 7.75 2.4502 16 2.4502C21.85 2.4502 26.95 5.7502 29.35 10.7002"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+        />
+        <path
+          d="M30.7 2L29.5 10.85L20.5 9.65"
+          stroke="#FFBB38"
+          strokeWidth="2"
+          strokeMiterlimit="10"
+          strokeLinecap="square"
+        />
+      </svg>
+    ),
+  },
+];
