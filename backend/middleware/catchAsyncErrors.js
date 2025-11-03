df280c5b8541a133789e55704a13993b525c829b
Gull Snobar <abdulrehmanali7775@gmail.com>
2025-11-03 12:45:11 +0500

    The code is pushed

--- backend/middleware/catchAsyncErrors.js
+++ backend/middleware/catchAsyncErrors.js
@@ -1,3 +1,6 @@
+// This is catchAsyncErrors middleware to handle errors in async functions
+
+
 module.exports = (theFunc) => (req, res, next) => {
   Promise.resolve(theFunc(req, res, next)).catch(next);
 };
