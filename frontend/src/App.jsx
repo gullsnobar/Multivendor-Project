e8c732a7d1d1d47d1d570354d09040946a7e9718
Gull Snobar <abdulrehmanali7775@gmail.com>
2025-11-08 23:58:49 +0500

    App.jsx Code Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage, SignupPage, ActivationPage } from "./Routes.js";
 
+
+// This is umcomplete right now first complete it.
 const App = () => {
   return (
     <BrowserRouter>
